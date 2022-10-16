1c40b197ef022e983ccfff462a8d0ffad3860de0
Lau Yee-Yu <liuyiyu2003@126.com>
2022-10-16 19:24:12 +0800

    chore: modify the gradle file to enable the antlr part

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     application
 }
 
-group = "org.example"
+group = "org.yeeyu.lau"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -14,6 +14,7 @@ repositories {
 }
 
 dependencies {
+    antlr("org.antlr:antlr4:4.11.1")
     testImplementation(kotlin("test"))
 }
 
@@ -22,7 +23,7 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "18"
 }
 
 application {
