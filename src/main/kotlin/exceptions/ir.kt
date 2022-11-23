f993f536e60847e5374fd587532df1f903db9d67
Lau Yee-Yu <liuyiyu2003@126.com>
2022-11-24 01:39:24 +0800

    feat: add exception class for IR

---
+++ src/main/kotlin/exceptions/ir.kt
@@ -0,0 +1,21 @@
+// Mx-Compiler - a compiler implementation for Mx
+// Copyright (C) 2022 Lau Yee-Yu
+//
+// This library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package exceptions
+
+class IRBuilderException(msg: String) : MxException(msg, null)
+
+class EnvironmentException(msg: String) : MxException(msg, null)
