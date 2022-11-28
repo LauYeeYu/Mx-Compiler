7709a9878a2d860f62ba67f0423af3ceafd7c6fe
Lau Yee-Yu <liuyiyu2003@126.com>
2022-11-28 15:27:45 +0800

    feat: add the escaping function for ir string

---
+++ src/main/kotlin/ir/utility.kt
@@ -0,0 +1,30 @@
+// Mx-Compiler - a compiler implementation for Mx
+// Copyright (C) 2022 Lau Yee-Yu
+//
+// This library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package ir
+
+fun escapeStringLiteralToIr(source: String): String {
+    val sb = StringBuilder()
+    for (c in source) {
+        when (c) {
+            '\n' -> sb.append("\\0A")
+            '\\' -> sb.append("\\\\")
+            '"' -> sb.append("\\\"")
+            else -> sb.append(c)
+        }
+    }
+    return sb.toString()
+}
