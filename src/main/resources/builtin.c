19f99d874cabe7776e7381e38fb9e086b3696984
Lau Yee-Yu <liuyiyu2003@126.com>
2023-01-18 21:32:54 +0800

    refactor: change the code style of builtin functions

--- src/main/resources/builtin.c
+++ src/main/resources/builtin.c
@@ -16,11 +16,11 @@ int strcmp(const char *s1, const char *s2);
 void *memcpy(void *dest, const void *src, size_t n);
 void *malloc(size_t n);
 
-void print(char* str) {
+void print(char *str) {
     printf("%s", str);
 }
 
-void println(char* str) {
+void println(char *str) {
     printf("%s\n", str);
 }
 
@@ -32,8 +32,8 @@ void printlnInt(int n) {
     printf("%d\n", n);
 }
 
-char* getString() {
-    char* buffer = malloc(4096);
+char *getString() {
+    char *buffer = malloc(4096);
     scanf("%s", buffer);
     return buffer;
 }
@@ -44,97 +44,97 @@ int getInt() {
     return n;
 }
 
-char* toString(int n) {
-    char* buffer = malloc(16);
+char *toString(int n) {
+    char *buffer = malloc(16);
     sprintf(buffer, "%d", n);
     return buffer;
 }
 
-char* string_string() {
-    char* buffer = malloc(1);
+char *string_string() {
+    char *buffer = malloc(1);
     buffer[0] = '\0';
     return buffer;
 }
 
-size_t string_length(char* __this) {
+size_t string_length(char *__this) {
     return strlen(__this);
 }
 
-char* string_substring(char* __this, int left, int right) {
+char *string_substring(char *__this, int left, int right) {
     int length = right - left;
-    char* buffer = malloc(length + 1);
+    char *buffer = malloc(length + 1);
     memcpy(buffer, __this + left, length);
     buffer[length] = '\0';
     return buffer;
 }
 
-int string_parseInt(char* __this) {
+int string_parseInt(char *__this) {
     int n;
     sscanf(__this, "%d", &n);
     return n;
 }
 
-int string_ord(char* __this, int pos) {
+int string_ord(char *__this, int pos) {
     return __this[pos];
 }
 
-char* string_add(char* str1, char* str2) {
+char *string_add(char *str1, char *str2) {
     int length1 = strlen(str1);
     int length2 = strlen(str2);
     int length = length1 + length2;
-    char* buffer = malloc(length + 1);
+    char *buffer = malloc(length + 1);
     memcpy(buffer, str1, length1);
     memcpy(buffer + length1, str2, length2);
     buffer[length] = '\0';
     return buffer;
 }
 
-bool string_equal(char* str1, char* str2) {
+bool string_equal(char *str1, char *str2) {
     return strcmp(str1, str2) == 0;
 }
 
-bool string_notEqual(char* str1, char* str2) {
+bool string_notEqual(char *str1, char *str2) {
     return strcmp(str1, str2) != 0;
 }
 
-bool string_less(char* str1, char* str2) {
+bool string_less(char *str1, char *str2) {
     return strcmp(str1, str2) < 0;
 }
 
-bool string_lessOrEqual(char* str1, char* str2) {
+bool string_lessOrEqual(char *str1, char *str2) {
     return strcmp(str1, str2) <= 0;
 }
 
-bool string_greater(char* str1, char* str2) {
+bool string_greater(char *str1, char *str2) {
     return strcmp(str1, str2) > 0;
 }
 
-bool string_greaterOrEqual(char* str1, char* str2) {
+bool string_greaterOrEqual(char *str1, char *str2) {
     return strcmp(str1, str2) >= 0;
 }
 
-int array_size(void* __this) {
+int array_size(void *__this) {
     return ((int*)__this)[-1];
 }
 
-void* __newClassArray(int size) {
+void *__newClassArray(int size) {
 #ifdef HOST
-    int* array = malloc((size << 3) + 4);
+    int *array = malloc((size << 3) + 4);
 #else
-    int* array = malloc((size << 2) + 4);
+    int *array = malloc((size << 2) + 4);
 #endif
     array[0] = size;
     return array + 1;
 }
 
-void* __newIntArray(int size) {
-    int* array = malloc((size << 2) + 4);
+void *__newIntArray(int size) {
+    int *array = malloc((size << 2) + 4);
     array[0] = size;
     return array + 1;
 }
 
-void* __newBoolArray(int size) {
-    int* array = malloc(size + 4);
+void *__newBoolArray(int size) {
+    int *array = malloc(size + 4);
     array[0] = size;
     return array + 1;
 }
