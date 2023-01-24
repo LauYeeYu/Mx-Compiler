bbafb28115b289cc47c4c5bd88b1060696734b7d
Lau Yee-Yu <liuyiyu2003@126.com>
2023-01-24 23:10:15 +0800

    feat(asm): register status tools

--- src/main/kotlin/asm/utility.kt
+++ src/main/kotlin/asm/utility.kt
@@ -34,3 +34,38 @@ fun buildGlobalVariable(variable: ir.GlobalDecl): GlobalVariable = when(variable
         body = listOf(StringLiteral(escapeStringLiteralToAsm(variable.content))),
     )
 }
+
+enum class RegStatus {
+    FREE, // Nothing is stored in this register, or the data is stored in memory
+    OCCUPIED, // Something is stored in this register, but not in the memory
+}
+
+fun buildInitRegisterStatus() = mutableMapOf(
+    "t0" to RegStatus.FREE,
+    "t1" to RegStatus.FREE,
+    "t2" to RegStatus.FREE,
+    "t3" to RegStatus.FREE,
+    "t4" to RegStatus.FREE,
+    "t5" to RegStatus.FREE,
+    "t6" to RegStatus.FREE,
+    "s0" to RegStatus.OCCUPIED,
+    "s1" to RegStatus.OCCUPIED,
+    "s2" to RegStatus.OCCUPIED,
+    "s3" to RegStatus.OCCUPIED,
+    "s4" to RegStatus.OCCUPIED,
+    "s5" to RegStatus.OCCUPIED,
+    "s6" to RegStatus.OCCUPIED,
+    "s7" to RegStatus.OCCUPIED,
+    "s8" to RegStatus.OCCUPIED,
+    "s9" to RegStatus.OCCUPIED,
+    "s10" to RegStatus.OCCUPIED,
+    "s11" to RegStatus.OCCUPIED,
+    "a0" to RegStatus.FREE,
+    "a1" to RegStatus.FREE,
+    "a2" to RegStatus.FREE,
+    "a3" to RegStatus.FREE,
+    "a4" to RegStatus.FREE,
+    "a5" to RegStatus.FREE,
+    "a6" to RegStatus.FREE,
+    "a7" to RegStatus.FREE,
+)
